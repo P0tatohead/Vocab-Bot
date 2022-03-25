87789fdf4115eb02b34863fa1f9f4d9633e47966
P0tatohead <64038795+P0tatohead@users.noreply.github.com>
2022-03-25 08:33:46 -0700

    Update vocab script.py

--- vocab script.py
+++ vocab script.py
@@ -4,7 +4,8 @@ from PyDictionary import PyDictionary as pyDict
 
 done = True
 
-
+#The getWiki() function uses the wikipedia library to get the definition of the string input
+#Later in the getDef() function, the number of spaces in the term gets the number of lines form the summary (i know i'm so smart §(*￣▽￣*)§)
 def getWiki(command, lines):
     
     try:
@@ -15,16 +16,18 @@ def getWiki(command, lines):
         
         return 0
 
+#The getDict function uses the PyDictionary libtary to get the definition of the string input
 def getDict(word):
     
     try:
         
-        defintion = pyDict.meaning(word)
+        definition = pyDict.meaning(word)
         return definition
     except:
         
         return 0
 
+#Pretty simple, just uses the open(function) and prints the inputed string into the defintions.txt file
 def writeToFile(output):
     
     with open("definitions.txt", "a", encoding = "utf-8") as textFile:
@@ -36,6 +39,7 @@ def writeToFile(output):
         textFile.write(" ")
         textFile.write("\n")
 
+#The getDef() functions takes a string input and outputs the defintion
 def getDef(search):
     
         lines = search.count(" ")
@@ -48,6 +52,7 @@ def getDef(search):
 
         writeToFile(definition)
 
+#Using the open() functions, the getVocab() function downloads all the vocab into a list and outputs it
 def getVocab():
 
     with open("vocab.txt", "r") as vocab:
@@ -56,6 +61,7 @@ def getVocab():
         print (terms)
         return terms
 
+#final function that get the list from getVocab and inputs into the getDef() functions
 def giveDefs():
 
     vocab = getVocab()
